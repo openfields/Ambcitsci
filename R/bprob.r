9baab28169b551fb3a0e3d8d5bc03e14f1d08cd2
William Fields <will.openfields@gmail.com>
2016-05-04 10:46:36 -0400

    function to determine who breeds/skips breeding

---
+++ R/bprob.r
@@ -0,0 +1,9 @@
+# bprob.r: function that uses breeding probability to split population into breeders and nonbreeders at start of breeding season
+# input: population size, breeding probability (1 - temporary emigration; estimated from multistate mark-recapture model)
+# processing: determine whether each adult breeds or doesn't breed
+# output: return vector with 1/0 for breeding/nonbreeding
+bprob <- function(pop, prob){
+ out <- rbinom(pop, 1, prob)
+ return(out)
+# could modify function to deal with Markovian temporary emigration
+}

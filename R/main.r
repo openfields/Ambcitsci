7a5d7f461107ec73dd2da5f59e9a00afb49f74f1
William Fields <will.openfields@gmail.com>
2016-05-09 17:09:37 -0400

    main function to call others for simulation

---
+++ R/main.r
@@ -0,0 +1,16 @@
+# simulation of maculatum population:
+# 1. initial adults
+# need to use survival function
+# 2. breeding decision: bprob.r
+source("~/Documents/bignight/R/bprob.r")
+# 3. inbound migration
+source("~/Documents/bignight/R/migrate.r")
+# 4. fecundity: use to produce number of metamorphs
+source("~/Documents/bignight/R/fecund.r")
+# 5. outbound migration
+# use migrate.r again
+# 6. nonbreeding survival
+source("~/Documents/bignight/R/surv.r")
+# 7. metamorph survival
+# need to use survival functions
+

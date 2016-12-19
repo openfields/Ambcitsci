87e26bc55ea1bff2e60304cf93a368b86c850755
Will Fields <will.openfields@gmail.com>
2016-12-19 10:30:18 -0500

    fixed functiont to calculate lambda and return list

--- R/lampop.r
+++ R/lampop.r
@@ -2,6 +2,6 @@ lampop <- function(vec){
   # calculate lambda from time series: divide 2nd through last element by 1st through 2nd to last element
   tmp1 <- vec[1:length(vec)-1]
   tmp2 <- vec[2:length(vec)]
-  tmp2/tmp2 -> lam
-  return(lam=lam, vlam=var(lam))
+  tmp2/tmp1 -> lam
+  return(list(lam=lam, vlam=var(lam)))
 }

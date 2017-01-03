915933fdc5bfaf46bfad25914b39d37250ef6aef
Will Fields <will.openfields@gmail.com>
2017-01-03 05:35:41 -0500

    changed parameter name and added line to sum across stages (column) at each row (time step)

--- R/lampop.r
+++ R/lampop.r
@@ -1,7 +1,8 @@
-lampop <- function(vec){
+lampop <- function(dat){
   # calculate lambda from time series: divide 2nd through last element by 1st through 2nd to last element
-  tmp1 <- vec[1:length(vec)-1]
-  tmp2 <- vec[2:length(vec)]
+  dat <- apply(dat,1,sum)
+  tmp1 <- dat[1:length(dat)-1]
+  tmp2 <- dat[2:length(dat)]
   tmp2/tmp1 -> lam
   return(list(lam=lam, vlam=var(lam)))
 }

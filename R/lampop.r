31da0dadba63d807c2873aa87aa5547525bfc1de
Will Fields <will.openfields@gmail.com>
2017-01-03 05:48:57 -0500

    added condition to test for quasi-ext and change how lam/var(lam) are calculated

--- R/lampop.r
+++ R/lampop.r
@@ -4,9 +4,13 @@ lampop <- function(dat,qe){
   dat <- apply(dat,1,sum)
   # set quasi-ext threshold
   dat[which(dat<qe)] <- 0
+  dat[which(dat==0)] -> extind
   
   tmp1 <- dat[1:length(dat)-1]
   tmp2 <- dat[2:length(dat)]
   tmp2/tmp1 -> lam
+  if(length(which(dat==0))>=1) {
+    lam <- lam[1:extind[1]]
+  }
   return(list(lam=lam, vlam=var(lam)))
 }

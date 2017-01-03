0e6a8f6928ae17d5368f625fde9ef421b4d8e7cb
Will Fields <will.openfields@gmail.com>
2017-01-03 05:39:26 -0500

    added quasi-ext thresh parameter, zeroed vector for total pop

--- R/lampop.r
+++ R/lampop.r
@@ -1,6 +1,10 @@
-lampop <- function(dat){
+lampop <- function(dat,qe){
   # calculate lambda from time series: divide 2nd through last element by 1st through 2nd to last element
+  # sum across time steps to get total population
   dat <- apply(dat,1,sum)
+  # set quasi-ext threshold
+  dat[which(dat<qe)] <- 0
+  
   tmp1 <- dat[1:length(dat)-1]
   tmp2 <- dat[2:length(dat)]
   tmp2/tmp1 -> lam
